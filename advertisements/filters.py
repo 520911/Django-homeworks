b232e08f2b08847889f4ee8e337e6b26c680806c
520911 <denisrudakov89@gmail.com>
2021-09-03 09:39:42 +0400

    Fixed status

--- advertisements/filters.py
+++ advertisements/filters.py
@@ -11,4 +11,4 @@ class AdvertisementFilter(filters.FilterSet):
 
     class Meta:
         model = Advertisement
-        fields = ['created_at', 'updated_at', 'creator']
+        fields = ['status', 'created_at', 'updated_at', 'creator']
